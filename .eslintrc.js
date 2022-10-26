9fa713b079126976f8af4db618a6f25cc4d35eb8
MauricioCorzo <mauricio.corzo@yahoo.com>
2022-10-26 16:32:08 -0300

    Cambios

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
         sourceType: 'module',
     },
     plugins: ['@typescript-eslint'],
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'], // 'plugin:prettier/recommended', me generaba el error del cr
     root: true,
     env: {
         node: true,
